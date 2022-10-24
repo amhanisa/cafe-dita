ccc6cc46f2578f625d7a71b6465d70e86399cf2e
amhanisa <azka@amhanisa.com>
2022-10-24 17:11:40 +0700

    create habit data structure

--- database/migrations/2022_10_24_031539_create_habits_table.php
+++ database/migrations/2022_10_24_031539_create_habits_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('patient_habit', function (Blueprint $table) {
+        Schema::create('habits', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('patient_habit');
+        Schema::dropIfExists('habits');
     }
 };
---
+++ database/migrations/2022_10_24_032237_create_patient_habits_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\Habit;
+use App\Models\Patient;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient_habits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Patient::class);
+            $table->foreignIdFor(Habit::class);
+            $table->year('year');
+            $table->integer('month');
+            $table->boolean('week1')->nullable();
+            $table->boolean('week2')->nullable();
+            $table->boolean('week3')->nullable();
+            $table->boolean('week4')->nullable();
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient_habits');
+    }
+};
