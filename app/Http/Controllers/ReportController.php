18d2d58906ff4cd986f4048a6ec055ac3c17e0c7
amhanisa <azka@amhanisa.com>
2022-10-30 14:29:28 +0700

    add function to get patients for report data

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function index(Request $request)
     {
+        // TODO: Set batas-batas yg memungkinkan dari 4 variabel ini
+        $startDate = $request->query('start_date') ?? '2010-01-01';
+        $endDate = $request->query('end_date') ?? Carbon::now()->format('Y-m-d');
+        $minAge = $request->query('min_age') ?? 0;
+        $maxAge = $request->query('max_age') ?? 100;
+
+        $patients = Patient::getPatientsForReport($startDate, $endDate, $minAge, $maxAge);
+
+        $this->calculateHipertension($patients);
+
         return view('report.index');
+    }
+
+    public function calculateHipertension($patients)
+    {
+        // TODO susah ni ga kepikiran ngitungnya gimana haha
     }
 }
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Patient extends Model
 {
@@ -14,5 +16,29 @@ class Patient extends Model
     public function consultation()
     {
         return $this->hasMany(Consultation::class);
+    }
+
+    public static function getPatientsForReport($startDate, $endDate, $minAge, $maxAge)
+    {
+        $query = <<<EOD
+        SELECT id FROM (
+            SELECT *, FLOOR (DATEDIFF(NOW(),birthday) /365.2425) AS 'age' FROM patients
+            WHERE id IN (
+                SELECT DISTINCT(patient_id) FROM consultations
+                WHERE DATE > "$startDate"
+                AND DATE < "$endDate"
+                ORDER BY patient_id
+            )
+        ) AS p
+        WHERE age > $minAge AND age < $maxAge
+        EOD;
+
+        $patientList = DB::select(DB::raw($query));
+
+        $patientsId = array_column($patientList, 'id');
+
+        return (new static)::with(['consultation' => function ($query) use ($endDate) {
+            $query->whereBetween('date', [Carbon::parse($endDate)->subYear(), $endDate])->orderBy('date', 'asc');
+        }])->whereIn('id', $patientsId)->get();
     }
 }
