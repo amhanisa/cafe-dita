5e227875ea35b2997a03940f49138338705a6a42
amhanisa <azka@amhanisa.com>
2022-11-15 17:10:57 +0700

    update sidebar design

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -17,52 +17,40 @@
     </div>
     <div class="sidebar-menu">
         <ul class="menu">
-            <li class="sidebar-item has-sub">
-                <a class="sidebar-link" href="#">
-                    <svg class="bi bi-person-circle" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                        fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                        <path fill-rule="evenodd"
-                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                    </svg>
-                    <span>
-                        Azka Muhammad as <br />
-                        <small>Administrator</small>
-                    </span>
-                </a>
-                <ul class="submenu">
-                    <li class="submenu-item">
-                        <a href="component-alert.html">
-                            <svg class="bi bi-gear-fill" xmlns="http://www.w3.org/2000/svg" width="16"
-                                height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path
-                                    d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
-                            </svg>
-                            <span>Pengaturan</span>
-                        </a>
-                    </li>
-                    <li class="submenu-item">
-                        <a href="component-badge.html">
+            <li class="sidebar-item">
+                <div class="flex-column">
+                    <div class="d-flex flex-row align-items-center mb-2">
+                        <svg class="bi bi-person-circle" style="width: 2rem; height: 2rem; fill:#7c8db5;"
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fill-rule="evenodd"
+                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg>
+                        <div class="ms-2">
+                            {{ Auth::user()->name }} <br />
+                            <small>{{ Auth::user()->role->name }}</small>
+                        </div>
+                    </div>
+                    <div>
+                        <form action="{{ url('logout') }}" method="post">
+                            @csrf
+                            <button class="btn btn-block btn-sm btn-outline-secondary" type="submit">
+                                <svg class="bi bi-box-arrow-left" xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
+                                    <path fill-rule="evenodd"
+                                        d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+                                </svg>
+                                Logout
+                            </button>
 
-                            <form action="{{ url('logout') }}" method="post">
-                                @csrf
-                                <button class="btn btn-secondary" type="submit">
-                                    <svg class="bi bi-box-arrow-left" xmlns="http://www.w3.org/2000/svg" width="16"
-                                        height="16" fill="currentColor" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd"
-                                            d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
-                                        <path fill-rule="evenodd"
-                                            d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
-                                    </svg>
-                                    Keluar
-                                </button>
-
-                            </form>
-                        </a>
-                    </li>
-                </ul>
+                        </form>
+                    </div>
+                </div>
             </li>
-            <img class="asd" src="" alt="">
+            <hr class="hr mt-4">
             <li class="sidebar-title">Menu</li>
 
             <li class="sidebar-item {{ request()->is('/') ? 'active' : '' }}">
@@ -125,14 +113,6 @@
                     </svg>
                     <span>Petugas</span>
                 </a>
-                <!-- <ul class="submenu">
-                    <li class="submenu-item">
-                        <a href="layout-default.html">Default Layout</a>
-                    </li>
-                    <li class="submenu-item">
-                        <a href="layout-vertical-1-column.html">1 Column</a>
-                    </li>
-                </ul> -->
             </li>
 
             <li class="sidebar-item">
