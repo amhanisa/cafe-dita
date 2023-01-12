e264546a32552e109414cb946e1afbadec29013a
amhanisa <azka@amhanisa.com>
2023-01-12 18:47:19 +0700

    Update FakeDataSeeder.php

--- database/seeders/FakeDataSeeder.php
+++ database/seeders/FakeDataSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class FakeDataSeeder extends Seeder
 {
     /**
      * Seed the application's database.
